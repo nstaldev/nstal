5f01fffd24d46dc61f6830c512b279bf45222e61
Philippe Bernard <philippe.bernard@gmail.com>
2022-04-29 14:42:43 +0200

    addCodeToFunction

--- src/commands/transform.test.ts
+++ src/commands/transform.test.ts
@@ -1,4 +1,4 @@
-import { addNamedImport } from "./transform";
+import { addCodeToFunction, addNamedImport } from "./transform";
 
 test('addNamedImport', () => {
   expect(addNamedImport(`
@@ -12,4 +12,24 @@ test('addNamedImport', () => {
 
   // TODO: Prevent duplicated imports
   // TODO: Add import to the end of the imports section
+});
+
+
+test('addCodeToFunction', () => {
+  expect(addCodeToFunction(`
+    // Hey!
+    function someFunc() {
+      console.log("Hello world");
+    }
+  `, 'someFunc', `
+    const x = 5;
+    console.log('X=' + x);
+  `)).toEqual(`
+    // Hey!
+    function someFunc() {
+      const x = 5;
+      console.log('X=' + x);
+      console.log("Hello world");
+    }
+  `);
 });
--- src/commands/transform.ts
+++ src/commands/transform.ts
@@ -1,4 +1,3 @@
-
 import { parse, print, types } from "recast";
 import { detectNewline } from "../utils";
 
@@ -13,7 +12,21 @@ export const addNamedImport = (source: string, name: string, from: string): stri
 
   code.program.body.unshift(importDeclaration);
 
-  const output = print(code, { lineTerminator: detectNewline(source) }).code;
-
-  return output;
+  return print(code, { lineTerminator: detectNewline(source) }).code;
 }
+
+export const addCodeToFunction = (source: string, funcName: string, extraSource: string): string => {
+  const code = parse(source);
+
+  const extraCode = parse(extraSource);
+
+  const func = findFunction(code.program.body, funcName);
+
+  func.body.body = extraCode.program.body.concat(func.body.body);
+
+  return print(code, { lineTerminator: detectNewline(source) }).code;
+}
+
+const findFunction = (programBody: any, functionName): any => (
+  programBody.find(node => node.type === 'FunctionDeclaration' && node.id.name === functionName)
+);
